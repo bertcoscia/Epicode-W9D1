cd925424d332913bdfac6c5ad2cf57772eb6d1b0
A. Alberto Coscia <angeloalberto.coscia@gmail.com>
2024-06-24 16:39:57 +0200

    adds ClipboardButton.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,6 @@
 import ImageComponent from "./ImageComponent";
 import ButtonComponent from "./ButtonComponent";
+import ClipboardButton from "./ClipboardButton";
 
 const Card = props => {
   return (
@@ -8,7 +9,10 @@ const Card = props => {
       <div class="card-body">
         <p class="card-text">{props.cardTxt}</p>
       </div>
-      <ButtonComponent btnText="Open image in new tab" className="btn btn-success rounded-pill px-3 py-2 mb-3 align-self-center" url={props.imgSrc} />
+      <div className="container d-flex justify-content-between align-items-center">
+        <ButtonComponent btnText="Open image in new tab" className="btn btn-success rounded-pill px-3 py-2 mb-3 align-self-center" url={props.imgSrc} />
+        <ClipboardButton className="btn" url={props.imgSrc} />
+      </div>
     </div>
   );
 };
